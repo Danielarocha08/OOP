f68d4998b1daaa5f058ddb5a8aaa9fc0095a2be0
Danielarocha08 <161525815+Danielarocha08@users.noreply.github.com>
2024-04-03 19:41:46 -0500

    Add files via upload

---
+++ Java/AccesModifiers/Cars/Carrros.java
@@ -0,0 +1,27 @@
+package carrros;
+
+public class Carrros {
+    int modelYear;
+    String modelName;
+    int precio;
+    
+    public Carrros(int year, String name){
+        modelYear = year;
+        modelName = name;
+        precio = prize;
+    System.out.println("mi carro es modelo" + modelYear + "y es un" + modelName);    
+    }
+    
+    public int precio (int prize){
+        precio = prize;
+        return precio;
+        
+    }
+    public static void main(String[] args) {
+        Carrros myCar = new Carrros(2000,"logan");
+        System.out.println(myCar.precio(5000));
+    
+        
+    }
+    
+}
