4ef8a9528dc5d0a3287cc6d7c267bdcf6df0915d
Danielarocha08 <161525815+Danielarocha08@users.noreply.github.com>
2024-04-03 20:59:07 -0500

    Add files via upload

---
+++ Java/Herencia/Vehiculos/Car.java
@@ -0,0 +1,22 @@
+public class Car extends Vehicle{
+   int wheels = 4; // ruedas
+   int door = 4; // puertas
+   boolean doors;
+
+    public Car() {
+    }
+
+    public Car(boolean doors) {
+        this.doors = doors;
+    }
+
+    public void setDoors(boolean doors) {
+        this.doors = doors;
+    }
+    
+    
+   
+   
+   
+   
+}
