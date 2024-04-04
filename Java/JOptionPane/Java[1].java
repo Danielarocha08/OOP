70f82c019daa3ea5ca265cf12c1d34783b374079
Danielarocha08 <161525815+Danielarocha08@users.noreply.github.com>
2024-04-04 09:05:46 -0500

    Add files via upload

---
+++ Java/JOptionPane/Java[1].java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+
+public class Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Welcome\nto\nJava\nProgramming!");
+        
+            System.exit(0);
+        
+        // TODO code application logic here
+    }
+    
+}
